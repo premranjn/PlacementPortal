a469bdd4ef68f392b72b2c167ada6bca06fc5d1c
Prem Ranjan <82430644+premranjn@users.noreply.github.com>
2022-10-16 11:45:52 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,92 @@
+<?php
+    include "includes/header.php";
+?>
+		<div class="col-md-9 main" >
+		<!--banner-section-->
+            <div class="banner-section">
+            <!--/top-news-->
+            <div class="top-news">
+                <div class="about-content">
+                    <!-- about -->
+                    <h3 class="tittle">About <i class="glyphicon glyphicon-user"></i></h3>
+                    <img src="image_uploads/logo.png" alt="" />
+                    <p>The department of Training and Placement, the marketing division of the institute has these following functions and responsibilities:
+                        <label>Nurtures Industry Institute interaction, by organizing and coordinating frequent industrial visits, inplant training and projects of industrial relevance for the students, with the sole aim of zeroing down the hiatus between the industry and the academia.</label></p>
+                    <div class="clearfix"></div>
+                    <p class="nam">Helps every student define his/her career interest through individual expert counseling.
+Organizes and coordinates Campus Placement Program, to fulfill its commitment of a job to every aspirant.
+Over the years, the department has maintained symbiotic, vibrant and purposeful relationship with Industries across the country and as a result, has built up an impressive placement record both in terms of percentage of registered students placed, as well as the number of companies visiting the campus.
+Over 220 companies participate in the placement process every year. The institute enjoys best placements when it comes to PG programmes.
+ </p>
+
+                </div>
+                <!--div class="top-inner second">
+                    <div class="col-md-6 top-text">
+                        <a href="single.html"><img src="images/pic1.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="col-md-6 top-text two">
+                        <a href="single.html"><img src="images/pic2.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div-->
+            </div>
+            <!--//top-news-->
+        </div>
+    <!--//banner-section-->
+    <div class="banner-right-text">
+        <h3 class="tittle">Our Team <i class="glyphicon glyphicon-star"></i></h3>
+        <!--/general-news-->
+        <div class="general-news">
+            <div class="general-inner">
+                <div class="edit-pics">
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f4.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">NITT</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">NITT</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">NITT</a></h5>
+                            <div class="td-post-date two"> </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--//general-news-->
+        <!--/news-->
+        <!--/news-->
+    </div>
+    </div>
+    <div class="clearfix"> </div>
+<?php
+include "includes/footer.php";
+?>
---
+++ includes/footer.php
@@ -0,0 +1,36 @@
+<!--/footer-->
+<div class="footer">
+    <div class="footer-top">
+        <div class="col-md-4 footer-grid">
+            <h4>PLACEMENT Management System</h4>
+            <ul class="bottom">
+                <li>Made with ❤️ by 6 Appeal </li>
+            </ul>
+        </div>
+        <div class="col-md-4 footer-grid">
+            
+        </div>
+        <div class="clearfix"> </div>
+    </div>
+</div>
+<div class="copy">
+</div>
+<div class="clearfix"> </div>
+<!--//footer-->
+<!--start-smooth-scrolling-->
+<script type="text/javascript">
+    $(document).ready(function() {
+        var defaults = {
+            containerID: 'toTop', // fading element id
+            containerHoverID: 'toTopHover', // fading element hover id
+            scrollSpeed: 1200,
+            easingType: 'linear'
+        };
+        $().UItoTop({ easingType: 'easeOutQuart' });
+    });
+</script>
+<a href="#home" id="toTop" class="scroll" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+
+
+</body>
+</html>
